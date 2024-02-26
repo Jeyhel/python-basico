ac3b7fa07c1e78fd51065ed216c39994a48f2fc3
Jeyhel <160855981+Jeyhel@users.noreply.github.com>
2024-02-26 09:24:49 -0500

    TUPLAS

---
+++ Tuplas.py
@@ -0,0 +1,34 @@
+nombre = input("ingrese el nombre: ")
+edad = input("Ingrese la edad: ")
+altura = input("Ingrese la altua: ")
+Informacion1 = tuple((nombre, edad, altura, ))
+print(Informacion1)
+
+pais = input("Escriba su pais: ")
+ciudad = input("Escriba su ciudad: ")
+barrio = input("Escriba su barrio: ")
+calle = input("ingrese su dirección: ")
+carrera = input("Ingrese la carrera: ")
+direccion = input("Ingrese la direccion de su casa: ")
+Informacion2 = tuple((pais, ciudad, barrio, calle, carrera, direccion))
+print(Informacion2)
+
+colegio = input("Escriba su colegio: ")
+tecnicos = input("Escriba su tecnico: ")
+titulo = input("Escriba su titulo: ")
+maestria = input("ingrese su maestria: ")
+doctorado = input("Ingrese la doctorado: ")
+informacion3 = tuple((colegio, tecnicos, titulo, maestria, doctorado))
+print(informacion3)
+
+
+añosDeExperiencia= input("Escriba sus añosDeExperiencia: ")
+empresas  = input("Escriba para la empresa que ha trabajado: ")
+habilidades = input("Escriba sus habilidades: ")
+capacidades = input("ingrese sus capacidades: ")
+informacion4 = tuple((añosDeExperiencia, empresas, habilidades, capacidades))
+print(informacion4)
+
+
+
+
